0a3eee082db126c8ba9e55df3a15ff39094ccf1f
TWhite10 <136852211+TWhite10@users.noreply.github.com>
2024-11-11 14:28:00 -0500

    http commit

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log('Hello World')
+const http = require('http');
+const hostname = '127.0.0.1';
+const port = 3000;
+
+//try out the two server functions and the output 
+// const server = http.createServer((req, res) => {
+//     res.statusCode = 200;
+//     res.setHeader('Content-Type', 'text/plain');
+//     res.end('Hello World!\n');
+//   }); 
+//server two 
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<h1 style="color: red">Hello World!</h1>');
+    res.write('<p>I wonder what else we can send...</p>');
+    res.end();
+  });
+  server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+  });
